d1747049cd0d756044d4dda3d328a17278fbc926
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-08-12 16:22:52 -0700

    add fragments

--- app.py
+++ app.py
@@ -7,28 +7,37 @@ from sotopia.database import EpisodeLog
 from haicosystem.utils.render import render_for_humans # type: ignore
 from haicosystemDemo.hai_stream import streamlit_rendering
 
-@st.cache_data
-def episode_list(tag: str) -> tuple[list[EpisodeLog], int]:
-    episode_list = EpisodeLog.find(EpisodeLog.tag == tag).all()
-    episode_list_len = len(episode_list)-1
-    return episode_list, episode_list_len
+def episode_list() -> None:
+    # Text input for episode number
+    tag_option = st.selectbox(
+        "Which tag do you want to see?",
+        ("haicosystem_debug", "haicosystem_trial_experiment_0"),
+        placeholder="Select contact method...",
+    )
+    st.write("You selected:", tag_option)
+    print("You selected:", tag_option)
+    episode_list = EpisodeLog.find(EpisodeLog.tag == tag_option).all()
+    episode_list_len = max(len(episode_list)-1, 0)
+    st.session_state.episode_list_len = episode_list_len
+    st.session_state.episode_list = episode_list
+
+@st.fragment()
+def display_episode() -> None:
+    if st.session_state.episode_list_len == 0:
+        st.write("No episode found.")
+    else:
+        # Select an episode
+        episode_number = st.text_input(f"Enter episode number (0-{st.session_state.episode_list_len}):", value="0")
+        episode = st.session_state.episode_list[int(episode_number)]  # type: ignore
+        st.write(f"Episode retrieved: {episode.pk}")
+
+        assert isinstance(episode, EpisodeLog)
+        messages = render_for_humans(episode)
+        streamlit_rendering(messages)
 
 st.title("HAICosystem Episode Rendering")
-# Text input for episode number
-tag_option = st.selectbox(
-    "Which tag do you want to see?",
-    ("haicosystem_debug", "haicosystem_trial_experiment_0"),
-    placeholder="Select contact method...",
-)
+st.session_state.episode_list_len = 0
+st.session_state.episode_list = []
+episode_list()
+display_episode()
 
-st.write("You selected:", tag_option)
-episode_list, episode_list_len = episode_list(tag_option)
-
-# Select an episode
-episode_number = st.text_input(f"Enter episode number (0-{episode_list_len}):", value="2")
-episode = episode_list[int(episode_number)]  # type: ignore
-st.write(f"Episode retrieved: {episode.pk}")
-
-assert isinstance(episode, EpisodeLog)
-messages = render_for_humans(episode)
-streamlit_rendering(messages)
